3841425fad7257c7ad5a15a7ebdfd9a238f17ba0
amanda-chan <luv.amandachan@gmail.com>
2023-10-26 23:18:52 +0800

    basic pages for login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars'); // templating engine for generating dynamic HTML and other markup in web applications
+const bodyParser = require('body-parser'); // handle various types of data in the request body
 const app = express(); // create Express server
 const port = 3000;
 
@@ -11,6 +12,10 @@ createTables();
 var hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// middleware to parse the HTTP request body, allowing you to read data submitted via POST requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // load routes
 const loginRoute = require('./routes/login');
--- routes/login.js
+++ routes/login.js
@@ -7,4 +7,22 @@ router.get('/', (req, res) => {
     res.render('login/login', { layout: false }); // render the login page
 });
 
+// handling GET requests for "/register"
+router.get('/register', (req, res) => {
+    res.render('login/register', { layout: false }); // render the login page
+});
+
+// handling POST requests for "/register"
+router.post('/register', (req, res, next) => { 
+
+    // get form details
+    const name = req.body.name;
+    const email = req.body.email;
+    const role = req.body.role;
+    const password = req.body.password;
+
+    // create account and retrieve details to be stored into db
+
+});
+
 module.exports = router;
